a80fc72d0a5a02af212be929efdb2d1cc24f0fc0
ky <kywj1997@gmail.com>
2021-03-10 12:08:50 +0800

    deploy

---
+++ precache-manifest.2fdf6ce91843ad7f13ae52b3f915ab81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce6852aed44d3eac516",
+    "url": "/morney-website/css/app.b6f14e2f.css"
+  },
+  {
+    "revision": "4e21ed72f04af081d258f76ace434d4b",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "dce6852aed44d3eac516",
+    "url": "/morney-website/js/app.a7155185.js"
+  },
+  {
+    "revision": "4428cb93a0814c0e479c",
+    "url": "/morney-website/js/chunk-vendors.f82a97f0.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "7caa50ac13d33977cf9e506472e89ae0",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
